b403aadc9493bb450266d9020c6437a2fa30c5b7
shoma <shoma.aptx4869@gmail.com>
2019-09-02 05:14:39 +0900

    Initial Commit

---
+++ Firebase-Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase-Chat
+//
+//  Created by 池内将真 on 2019/09/02.
+//  Copyright © 2019 ChatTest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
